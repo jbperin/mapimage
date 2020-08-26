bea32a033318b4bc90055c066519b7bfc6800b85
jbperin <jbperin@users.noreply.github.com>
2020-08-26 13:47:14 +0200

    add pictures to introduce the topic on forums

--- proto/mapimage.py
+++ proto/mapimage.py
@@ -14,7 +14,7 @@ def norm (xi, yi, xj, yj):
 
 alpha   = math.atan2(y1-y0, x1-x0)
 beta    = math.atan2(y2-y0, x2-x0)
-print (math.degrees(alpha), math.degrees(beta))
+# print (math.degrees(alpha), math.degrees(beta))
 
 sinalpha    = math.sin(alpha)
 cosalpha    = math.cos(alpha)
@@ -24,10 +24,10 @@ cosbeta     = math.cos(beta)
 divisor     = sinalpha*cosbeta - cosalpha*sinbeta
 
 r2prime     = (sinalpha*(xp-x0) - cosalpha*(yp-y0) ) / divisor
-print (r2prime)
+# print (r2prime)
 
 r1prime     = (cosbeta*(yp-y0) - sinbeta*(xp-x0) ) / divisor
-print (r1prime)
+# print (r1prime)
 
 L1          = norm (x0, y0, x1, y1)
 L2          = norm (x0, y0, x2, y2)
@@ -38,3 +38,24 @@ H_RATIO     = IMAGE_HEIGHT / L2
 Xpix, Ypix = r1prime * W_RATIO, r2prime * H_RATIO
 
 print (Xpix, Ypix)
+
+
+
+P0x, P0y = x0, y0
+P1x, P1y = x1, y1
+P2x, P2y = x2, y2
+
+Px, Py = xp, yp
+
+def det (ux, uy, vx, vy):
+    return ux*vy - uy*vx
+
+
+v1x, v1y = P1x-P0x, P1y - P0y
+v2x, v2y = P2x-P0x, P2y - P0y
+vx, vy = Px - P0x, Py - P0y
+
+a = (det (vx, vy, v2x, v2y)- det(P0x, P0y, v2x, v2y))/det(v1x, v1y, v2x, v2y)
+b = (det (vx, vy, v1x, v1y)- det(P0x, P0y, v1x, v1y))/det(v1x, v1y, v2x, v2y)
+
+print (a*IMAGE_WIDTH, b*IMAGE_HEIGHT)
