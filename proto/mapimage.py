e05d62c54985f47abd24aad81ea1a6b279a8277e
jbperin <jbperin@users.noreply.github.com>
2020-08-27 14:03:30 +0200

    works on presentation

--- proto/mapimage.py
+++ proto/mapimage.py
@@ -8,34 +8,21 @@ x1, y1 = 18, 9
 x2, y2 = 9, 14
 xp, yp = 12, 9
 
-def norm (xi, yi, xj, yj):
-    return math.sqrt((xj-xi)**2 + (yj-yi)**2)
+alpha                   = math.atan2(y1-y0, x1-x0)
+beta                    = math.atan2(y2-y0, x2-x0)
 
+cosalpha, sinalpha      =  math.cos(alpha), math.sin(alpha)
+cosbeta, sinbeta        =  math.cos(beta), math.sin(beta)
 
-alpha   = math.atan2(y1-y0, x1-x0)
-beta    = math.atan2(y2-y0, x2-x0)
-# print (math.degrees(alpha), math.degrees(beta))
+divisor                 = sinalpha*cosbeta - cosalpha*sinbeta
 
-sinalpha    = math.sin(alpha)
-cosalpha    = math.cos(alpha)
-sinbeta     = math.sin(beta)
-cosbeta     = math.cos(beta)
+r2prime                 = (sinalpha*(xp-x0) - cosalpha*(yp-y0) ) / divisor
+r1prime                 = (cosbeta*(yp-y0) - sinbeta*(xp-x0) ) / divisor
 
-divisor     = sinalpha*cosbeta - cosalpha*sinbeta
+W_RATIO                 = IMAGE_WIDTH / math.sqrt((x1-x0)**2 + (y1-y0)**2)          # norm (x0, y0, x1, y1)
+H_RATIO                 = IMAGE_HEIGHT / math.sqrt((x2-x0)**2 + (y2-y0)**2)         # norm (x0, y0, x2, y2)
 
-r2prime     = (sinalpha*(xp-x0) - cosalpha*(yp-y0) ) / divisor
-# print (r2prime)
-
-r1prime     = (cosbeta*(yp-y0) - sinbeta*(xp-x0) ) / divisor
-# print (r1prime)
-
-L1          = norm (x0, y0, x1, y1)
-L2          = norm (x0, y0, x2, y2)
-
-W_RATIO     = IMAGE_WIDTH / L1
-H_RATIO     = IMAGE_HEIGHT / L2
-
-Xpix, Ypix = r1prime * W_RATIO, r2prime * H_RATIO
+Xpix, Ypix              = r1prime * W_RATIO, r2prime * H_RATIO
 
 print (Xpix, Ypix)
 
